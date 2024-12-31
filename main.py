1a329dd9524d1e23d3e7db538b253f69c99bfed8
Achintha Ekanyake <achiekanayake2@gmail.com>
2024-12-31 17:06:21 +0530

    corrected class name and readme file update

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import Calculator
+import calculator
 import tkinter as tk
 import os
 
 def main():
     root = tk.Tk()
     root.eval('tk::PlaceWindow . center')
+    
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
     icon_path = os.path.join(script_dir, "icon.ico")
     root.iconbitmap(icon_path)
 
-    app=Calculator.WellcomeMessage(root)
+    app=calculator.WellcomeMessage(root)
     root.mainloop()
 
 if __name__ == "__main__":
